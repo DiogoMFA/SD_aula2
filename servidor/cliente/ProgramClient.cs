37333f3963602e40b31cf8c274043e1c6afc2864
diogo <al81442@alunos.utad.pt>
2026-03-03 14:54:49 +0000

    inicio

---
+++ servidor/cliente/ProgramClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+// Conectar ao servidor
+TcpClient client = new TcpClient("127.0.0.1", 13000);
+using NetworkStream stream = client.GetStream();
+Console.WriteLine("--- Chat iniciado (Cliente) ---");
+
+while (true)
+{
+    Console.Write("Tu: ");
+    string? mensagem = Console.ReadLine();
+
+    if (!string.IsNullOrEmpty(mensagem))
+    {
+        byte[] data = Encoding.ASCII.GetBytes(mensagem);
+        stream.Write(data, 0, data.Length);
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string resposta = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Servidor: " + resposta);
+    }
+}
---
+++ servidor/servidor/ProgramServer.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+TcpListener server = new TcpListener(IPAddress.Any, 13000);
+server.Start();
+Console.WriteLine("--- Aguardando Cliente no Servidor ---");
+
+using TcpClient client = server.AcceptTcpClient();
+using NetworkStream stream = client.GetStream();
+
+while (true)
+{
+    byte[] buffer = new byte[1024];
+    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+    if (bytesRead == 0) break;
+
+    string msgRecebida = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+    Console.WriteLine("Cliente: " + msgRecebida);
+
+    Console.Write("Resposta: ");
+    string? resposta = Console.ReadLine();
+    if (resposta == null) resposta = "OK";
+
+    byte[] msgEnviar = Encoding.ASCII.GetBytes(resposta);
+    stream.Write(msgEnviar, 0, msgEnviar.Length);
+}
